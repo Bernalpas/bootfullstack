02265e04b22f043d5441d2295b410440b53f4665
Berni <bernalpas@gmail.com>
2026-07-29 21:37:21 -0300

    Agregamos un archivo JavaScript

---
+++ clase03-css-intro/js/login.js
@@ -0,0 +1,11 @@
+console.log("Hola gente")
+
+
+function login() {
+    //este evento imprime en consola un texto
+    console.log("Hola gente, alguien hizo click")
+
+    //mostrar una aviso en el navegador
+    alert("Hola, soy una ventana")
+
+}

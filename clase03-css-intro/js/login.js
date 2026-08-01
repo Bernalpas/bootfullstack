b5222cd3efbe86887ebfe69ea22a51c2e2ca710e
Berni <bernalpas@gmail.com>
2026-07-31 21:36:02 -0300

    Agregamos BoxModel

--- clase03-css-intro/js/login.js
+++ clase03-css-intro/js/login.js
@@ -8,4 +8,19 @@ function login() {
     //mostrar una aviso en el navegador
     alert("Hola, soy una ventana")
 
+    //crear una variable que guarde el valor del input del email
+    var email = document.getElementById("email").value
+
+    //imprimimos en consola el valor del input
+    //console.log(email)
+
+    //imprimo el contenido del input del email
+    console.log(email)
+
+    // creamos una variable que guarde el valor del input del password
+    var password = document.getElementById("password").value
+
+    //imprimimos en consola el valor del input
+    console.log(password)
+
 }
